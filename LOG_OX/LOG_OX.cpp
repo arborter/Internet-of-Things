80c8e577b56e8004d626fcd07ceec9885b120956
arborter <110191602+arborter@users.noreply.github.com>
2024-10-28 15:26:53 -0500

    update

--- LOG_OX/LOG_OX.cpp
+++ LOG_OX/LOG_OX.cpp
@@ -4,7 +4,7 @@
 #include <FastLED.h>
 
 // Set credentials
-char SSID[] = "-";
+char SSID[] = "";
 char PSWD[] = "";
 
 byte MAC[6];
@@ -14,6 +14,7 @@ IPAddress ip;
 IPAddress gateway;
 IPAddress subnet;
 
+// SETUP
 void setup() {
   M5Cardputer.begin();
   M5.Lcd.setTextSize(1);
@@ -21,17 +22,19 @@ void setup() {
   M5.Lcd.print("_____________________________ LOG.OX");
 }
 
+// EXECUTION
 void loop(){
   M5.update();
-  delay(500);
-  M5.Lcd.print("CHECKING SSID, PWD \n");
+  delay(2000);
+  M5.Lcd.print("\n CHECKING SSID, PWD \n");
   WiFi.begin(SSID, PSWD);
-  delay(10000);
+  delay(3000);
 
   unsigned long startAttemptTime = millis();
 
   // If not automatically logged into a network 
-  // upon booting, a scanning for networks begins
+  // upon booting device, a network scan operation begins
+
   while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < 10000)
   {
     delay(1000);
@@ -39,32 +42,32 @@ void loop(){
     M5.Lcd.print(".");
   }
 
-  M5.Lcd.print("/n STATUS: /n");
+  M5.Lcd.print("\n STATUS: \n");
 
   // Notify user on the status of the auto WiFi connection
   // whether logged in or not.
 
   if (WiFi.status() == WL_CONNECTED){
-    M5.Lcd.print("CONNECTED \n");
+    M5.Lcd.print("\n CONNECTED \n");
 
     // IP of shield
     ip = WiFi.localIP();
-    M5.Lcd.print("IP: ");
+    M5.Lcd.print("\n IP: ");
     M5.Lcd.print(ip);
 
     // MAC of shield
     WiFi.macAddress(MAC);
-    M5.Lcd.print("\n MAC:");
+    M5.Lcd.print("\n MAC: ");
     M5.Lcd.print(MAC[5], HEX);
 
     // GATEWAY IP of shield
     gateway = WiFi.gatewayIP();
-    M5.Lcd.print("\n GATEWAY:");
+    M5.Lcd.print("\n GATEWAY: ");
     M5.Lcd.print(gateway);
 
     // SUBNET
     subnet = WiFi.subnetMask();
-    M5.Lcd.print("\n SUBNET MASK : ");
+    M5.Lcd.print("\n SUBNET MASK: ");
     M5.Lcd.print(subnet);
   } else {
     M5.Lcd.print("\n NOT CONNECTED \n");
@@ -81,8 +84,10 @@ void loop(){
   } else {
     delay(1000);
     // NUMBER of networks found
-    M5.Lcd.print("\n NETWORKS FOUND: \n");
+    M5.Lcd.print("\n \n NUMBER OF NETWORKS FOUND:");
     M5.Lcd.print(networks_found);
+    M5.Lcd.print("\n \n \n");
+    M5.Lcd.print("\n_____________________________");
   }
   delay(500);
 }
