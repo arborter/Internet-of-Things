8f1924ca5ca8624be98609c8de8c6e0710bbf9b0
arborter <110191602+arborter@users.noreply.github.com>
2024-10-28 11:49:52 -0500

    New program

---
+++ LOG_OX/LOG_OX.cpp
@@ -0,0 +1,93 @@
+#include "M5Cardputer.h"
+#include <WiFi.h>
+#include "M5Unified.h"
+#include <FastLED.h>
+
+
+char SSID[] = "-";
+char PSWD[] = "";
+
+const int menu_size = 3;
+
+byte MAC[6];
+
+// IP & IP gateway & IP subnet
+IPAddress ip;
+IPAddress gateway;
+IPAddress subnet;
+
+void setup() {
+  M5Cardputer.begin();
+  M5.Lcd.setTextSize(1);
+  M5.Lcd.setTextColor(TFT_GREEN);
+  M5.Lcd.print("_____________________________ LOG.OX");
+}
+
+void loop(){
+  M5.update();
+  delay(500);
+  M5.Lcd.print("\n CHECKING SSID, PWD \n");
+  WiFi.begin(SSID, PSWD);
+  delay(10000);
+
+  unsigned long startAttemptTime = millis();
+
+  // If not automatically logged into a network 
+  // upon booting, a scanning for networks begins
+
+  while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < 10000)
+  {
+    delay(1000);
+    // load icon
+    M5.Lcd.print(".");
+  }
+
+  M5.Lcd.print("\n STATUS: \n");
+
+  // Notify user on the status of the auto WiFi connection
+  // whether logged in or not.
+
+  if (WiFi.status() == WL_CONNECTED){
+    M5.Lcd.print("\n CONNECTED \n");
+
+    // IP of shield
+    ip = WiFi.localIP();
+    M5.Lcd.print("\n IP \n");
+    M5.Lcd.print(ip);
+
+    // MAC of shield
+    WiFi.macAddress(MAC);
+    M5.Lcd.print("\n MAC: /n");
+    M5.Lcd.print(MAC[5], HEX);
+
+    // GATEWAY IP of shield
+    gateway = WiFi.gatewayIP();
+    M5.Lcd.print("\n GATEWAY: \n");
+    M5.Lcd.print(gateway);
+
+    // SUBNET
+    subnet = WiFi.subnetMask();
+    M5.Lcd.print("SUBNET MASK : \n");
+    M5.Lcd.print(subnet);
+  } else {
+    M5.Lcd.print("\n NOT CONNECTED \n");
+  }
+  delay(5000);
+
+  // Auto scan for networks and notify how many networks were found, if any
+  int networks_found = WiFi.scanNetworks();
+
+  if (networks_found == 0){
+    delay(500);
+    M5.Lcd.print("\n NETWORKS NONE \n");
+    delay(1000);
+  } else {
+    delay(1000);
+    // NUMBER of networks found
+    M5.Lcd.print("\n NETWORKS FOUND: \n");
+    M5.Lcd.print(networks_found);
+  }
+  delay(500);
+}
+
+
---
+++ LOG_OX/readme.md
@@ -0,0 +1,7 @@
+LOG_OX
+- WiFi enabled device allowing user to test his connection.
+- Upon booting, the Cardputer uses credentials of SSID and Password set by the user, then, should connection either succeed or fail, the screen will log:
+
+1. Status of connection
+2. Should the user connect, the log shows the IP address of the Cardputer, its MAC address, its GATEWAY IP address, and its Subnet mask.
+3. In all cases, the Cardputer will scan, then log the amount of WiFi networks found.
